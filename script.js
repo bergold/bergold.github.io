2d2f4f760b377d0a8ea1a7967b0b3f9a9f1f862c
bergold <emil.bergold@outlook.com>
2014-04-17 12:28:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var ghHome = angular.module('ghHome', []);
 
-ghHome.factory('repos', function($http) {
+/*ghHome.factory('repos', function($http) {
   return {
     getMy: function(un) {
       return $http.get('https://api.github.com/users/' + un + '/repos').success(function(data, status) {
@@ -8,14 +8,14 @@ ghHome.factory('repos', function($http) {
       });
     }
   };
-});
+});*/
 
-ghHome.controller('ReposCtrl', function($scope, repos) {
+ghHome.controller('ReposCtrl', function($scope) {
   $scope.repos = [];
   $scope.goto = function(repo) {
   
   };
-  repos.getMy('bergold').then(function(r) {
+  /*repos.getMy('bergold').then(function(r) {
     console.log(r);
-  });
+  });*/
 });
