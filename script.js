fe590d9a85f0fbb3f20e0a02d3917274dabca96e
bergold <emil.bergold@outlook.com>
2014-04-17 22:31:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,13 +15,6 @@ ghHome.factory('repos', function($q, $http) {
 ghHome.controller('ReposCtrl', function($scope, repos) {
   $scope.repos = [];
   repos.getPublic('bergold').then(function(rs) {
-    $scope.repos.push({
-      "name": "kaetering.bplaced.com",
-      "description": "My main server",
-      "homepage": "http://kaetering.bplaced.com"
-    });
-    angular.forEach(rs, function(r, i) {
-      $scope.repos.push(r);
-    });
+    $scope.repos = $scope.repos.concat(rs);
   });
 });
