2500cfc9b678e48330e441520dc301efa7a05411
bergold <emil.bergold@outlook.com>
2014-04-17 12:25:11 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+var ghHome = angular.module('ghHome', []);
+
+ghHome.factory('repos', function($http) {
+  return {
+    getMy: function(un) {
+      return $http.get('https://api.github.com/users/' + un + '/repos').success(function(data, status) {
+        return data;
+      });
+    }
+  };
+});
+
+ghHome.controller('ReposCtrl', function($scope, repos) {
+  $scope.repos = [];
+  $scope.goto = function(repo) {
+  
+  };
+  repos.getMy('bergold').then(function(r) {
+    console.log(r);
+  });
+});
